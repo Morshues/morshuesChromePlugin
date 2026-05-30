c8d5cea9aa312b530d044ed3b76a8cb6451c988e
Morshues Lai <morshues@gmail.com>
2026-05-30 18:31:09 +0800

    Update the answer crawler of animategamer

--- func/animategamer/script.js
+++ func/animategamer/script.js
@@ -3,25 +3,6 @@ let ifClose = false;
 const MAX_SCROLL_LIMIT = 30;
 let scrollCount = 0;
 
-function findNodeByRegex(blocks, regexText, useInnerText = false) {
-  // var aTags = parent.getElementsByTagName(tagName);
-  var found;
-
-  cmdRegex = RegExp(regexText, 'g');
-  for (var i = 0; i < blocks.length; i++) {
-    text = blocks[i].textContent;
-    if (useInnerText) {
-      text = blocks[i].innerText;
-    }
-    if (cmdRegex.test(text)) {
-      found = blocks[i];
-      break;
-    }
-  }
-
-  return found;
-}
-
 /* Find */
 function find_block() {
   if (scrollCount >= MAX_SCROLL_LIMIT) {
@@ -41,7 +22,7 @@ function find_block() {
   }
 
   let targetNode = dayNode;
-  while (targetNode != document.body) {
+  while (targetNode !== document.body) {
     if (targetNode.textContent.includes("檢視另")
       || targetNode.textContent.includes("查看更多留言")
       || targetNode.textContent.includes("more comments")
@@ -51,7 +32,7 @@ function find_block() {
     targetNode = targetNode.parentNode;
   }
 
-  if (targetNode == document.body) {
+  if (targetNode === document.body) {
     console.error("targetNode not found")
     return;
   }
@@ -73,7 +54,7 @@ function click_comment(targetNode) {
 }
 
 function scan_max(targetNode) {
-  const comments = Array.from(targetNode.querySelectorAll("div:not(:has(*))"))
+  const comments = Array.from(document.querySelectorAll('[role=article] span[lang]'))
     .filter(el => (el.textContent >= 1 && el.textContent <= 4));
 
   if (comments.length < 3) {
@@ -81,24 +62,23 @@ function scan_max(targetNode) {
     return;
   }
 
-  count_array = [null, 0, 0, 0, 0];
-  for (var i = 0; i < comments.length; i++) {
-    content_block = comments[i];
+  const count_array = [null, 0, 0, 0, 0];
+  for (let i = 0; i < comments.length; i++) {
+    const content_block = comments[i];
     if (content_block == null) {
       continue;
     }
-    value = parseInt(content_block.textContent);
+    const value = parseInt(content_block.textContent);
     count_array[value]++;
   }
-  max_index = null;
-  temp_value = 1;
-  for (var i = 1; i < 5; i++) {
+  let max_index = null;
+  let temp_value = 1;
+  for (let i = 1; i < 5; i++) {
     if (count_array[i] > temp_value) {
       max_index = i;
       temp_value = count_array[i];
     }
   }
-  max_index
   return answer_get(max_index);
 }
 
@@ -133,14 +113,14 @@ function paste_block() {
 }
 
 function auto_answer() {
-  quiz_block = document.getElementsByClassName('anime-quiz')[0]
+  const quiz_block = document.getElementsByClassName('anime-quiz')[0]
   if (quiz_block != null) {
-    quiz_btns = quiz_block.getElementsByTagName('a');
-    if (quiz_btns.length == 6) {
+    const quiz_btns = quiz_block.getElementsByTagName('a');
+    if (quiz_btns.length === 6) {
       quiz_btns[dataModel.answer].click();
       setTimeout(auto_answer, 3000);
       return;
-    } else if (quiz_btns.length == 1){
+    } else if (quiz_btns.length === 1){
       quiz_btns[0].click();
       return;
     }    
@@ -158,7 +138,7 @@ function auto_close() {
 
 /* Main Method */
 dataModel.load(function() {
-  if (document.URL.match(/https\:\/\/www\.facebook\.com\/p?g?\/?animategamer\.*/i)) {
+  if (document.URL.match(/https:\/\/www\.facebook\.com\/p?g?\/?animategamer\.*/i)) {
     if (dataModel.enable && !dataModel.isSameDay()) {
       find_block();
       auto_close();
